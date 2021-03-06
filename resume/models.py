cb1e3ed30b193f314dd04fc795be4b6c407468fa
Aveline Wingfield <avelinewingfield@gmail.com>
2021-03-05 20:21:18 -0500

    Fixed

--- resume/models.py
+++ resume/models.py
@@ -21,3 +21,6 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
